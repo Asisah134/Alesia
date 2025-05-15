7d273961607b7bc4e9b06a14e3c4f91f1baed165
Asisah134 <asisahnurul234@gmail.com>
2025-05-15 21:05:42 +0700

    Add files via upload

---
+++ alesiasoalno2.js
@@ -0,0 +1,9 @@
+// Problem 2 - Menghitung Luas Permukaan Tabung
+let T = 20; //tinggi tabung
+let r = 4;   //jari-jari
+const pi = 3.14; // Menggunakan nilai pi = 3.14
+
+// Menghitung luas permukaan tabung menggunakan rumus: 2 * pi * r * (r + T)
+let luas = 2 * pi * r * (r + T);
+
+console.log(luas);
